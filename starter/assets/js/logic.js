b1f43a76ef6808fe616b58f76cbdaca4a720434f
jsneath <jamessneath1@gmail.com>
2023-11-04 12:12:31 +0000

    add timer funtion on user clicking start quiz

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,7 +1,40 @@
 // When i click on start Quiz, the timer starts and 1st set of questions appear
-
-// create an array of questions and answers
+var startQuiz = document.querySelector("#start")
+var myQuestions = document.querySelector("#question-title")
+var startScreen = document.querySelector("#start-screen")
+var timer = document.querySelector("#time")
 // add an event listener for when the user selects start quiz
+
+
+
+
+startQuiz.addEventListener("click", function(event) {
+    startScreen.style.display = 'none';
+    startTimer(120, timer)
+    }
+)
+
+// I obtained help this timer function using google/chat gpt and got this to work in my code. These appear to be a regular requirement in JS. 
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    var interval = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            clearInterval(interval);
+            // You can call a function here to handle what happens when the timer ends
+            // e.g., endQuiz();
+        }
+    }, 1000);
+}
+
 // do a for loop through all questions array. Questions/answers stored as object?
 // if statement for correct answer 0 points - timer reduction
 // onto next question
