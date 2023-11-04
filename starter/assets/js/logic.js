d0b402f5a80dabebae97ad0af530975d1ddae585
jsneath <jamessneath1@gmail.com>
2023-11-04 11:33:12 +0000

    added array of example test questions

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,13 @@
+// When i click on start Quiz, the timer starts and 1st set of questions appear
+
+// create an array of questions and answers
+// add an event listener for when the user selects start quiz
+// do a for loop through all questions array. Questions/answers stored as object?
+// if statement for correct answer 0 points - timer reduction
+// onto next question
+
+// if user clicks correct answer add 1 point to users score
+// if user clicks incorrect answer - minus 10 seconds from timer & 0 points score
+// when timer reaches 0 the quiz stops 
+// the user is presented with their score and to enter their name
+// The users score is updated to high scores and stored
---
+++ starter/assets/js/questions.js
@@ -0,0 +1,54 @@
+
+
+// create an array of questions and answers
+
+var questions = [
+    {
+        question: "What is 4 + 8",
+        answers: ["11",
+            "12",
+            "48",
+            "4848484"],
+            correctAnswerIndex: 1
+    },
+    {
+        question: "What is 10 - 5",
+        answers: ["11",
+            "5",
+            "48",
+            "4848484"],
+            correctAnswerIndex: 1
+    },
+    {
+        question: "What is 10 - 4",
+        answers: ["44",
+            "12",
+            "48",
+            "6"],
+            correctAnswerIndex: 3
+    },
+    {
+        question: "What is 10 - 3",
+        answers: ["7",
+            "12",
+            "48",
+            "4848484"],
+            correctAnswerIndex: 0
+    },
+    {
+        question: "What is 15 - 5",
+        answers: ["11",
+            "12",
+            "10",
+            "4848484"],
+            correctAnswerIndex: 2
+    },
+    {
+        question: "What is 20 - 5",
+        answers: ["11",
+            "15",
+            "48",
+            "4848484"],
+            correctAnswerIndex: 1
+    },
+]
